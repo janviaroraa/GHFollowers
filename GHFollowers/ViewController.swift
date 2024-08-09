cb44778c40ecc3754ee0dceffe3bd8e242dfc6fc
janvi-aroraa <jjanvi.arora18@gmail.com>
2024-08-09 19:02:34 +0530

    Initial setup

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Janvi Arora on 09/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
